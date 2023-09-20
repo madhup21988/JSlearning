7152d915b3441596a19fe61b3d7c8e8803a8d12b
madhup21988 <madhup21988@gmail.com>
2023-09-20 08:58:38 +0530

    variable declaration
    variable declaration

---
+++ 01_swapVariable.js
@@ -0,0 +1,11 @@
+//WAP to swap variable values using third variariabl
+var  num1="100";
+var num2="200";
+var num3;
+
+ num3=num1;
+ num1=num2;
+ num2=num3;
+ console.log("number1 value",num1);
+ console.log("number2 value",num2);
+ console.log("number3 value",num3);
---
+++ 01_variable.js
@@ -0,0 +1,16 @@
+var firstName; // variable declaration
+firstName = "Madhuri";  // Variable initializatio
+console.log(firstName);
+firstName = "Mrs. Madhuri"; // Variable update
+console.log(firstName);
+
+
+var lastName = "Phadtare"; // variable declaration and initialization
+console.log(lastName);
+
+var height; // Variable Declaration
+console.log(height);
+height = "6"; // initialization
+console.log(height);
+height = "6.2 feet";// update
+console.log(height);
