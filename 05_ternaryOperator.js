880db04696bb6eb2f398ce00727df9775cb5fc4b
madhup21988 <madhup21988@gmail.com>
2023-09-27 13:06:51 +0530

    js learning

---
+++ 05_ternaryOperator.js
@@ -0,0 +1,13 @@
+var marks=62;
+//if marks grater than equal 60 then  allow for interview else dont allow
+
+marks >=60 ? "then allow": "dont allow";
+
+
+var num=4;
+num%4==0 ? "even": "odd";
+
+// If number is even then return "EVEN" else "ODD"
+var num = 13;
+var result = num%2 == 0 ? "EVEN" : "ODD";
+console.log(`Given number is ${num} ==> ${result}`);
